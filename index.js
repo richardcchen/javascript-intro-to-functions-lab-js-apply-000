a2a1b4ee8c9098ec26d049e9c543c81ee4ec44ff
Richard Chen <richard.c.chen@gmail.com>
2018-07-04 19:22:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowercase){
+  if (string === string.toLowercase()){
     return "I can't hear you!"
   }
 }
